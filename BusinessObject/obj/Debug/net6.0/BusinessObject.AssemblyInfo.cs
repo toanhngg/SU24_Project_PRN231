1f0962370cbbf491ed2d5cf8625001d3ccd39f17
FUS1310 <luongphunguyen1310@gmail.com>
2024-07-15 15:18:40 +0700

    add VNPayControllerAPI and add LibVNPay class and appsettings

--- BusinessObject/obj/Debug/net6.0/BusinessObject.AssemblyInfo.cs
+++ BusinessObject/obj/Debug/net6.0/BusinessObject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusinessObject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03265f5b7645fe328dc5726b0aff6668e150f255")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusinessObject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusinessObject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Project_API/Lib/PayLib.cs
@@ -0,0 +1,255 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System.Globalization;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+
+namespace Project_API.Lib
+{
+    public class Util
+    {
+        public static string Md5(string sInput)
+        {
+            HashAlgorithm algorithmType = default;
+            ASCIIEncoding enCoder = new ASCIIEncoding();
+            byte[] valueByteArr = enCoder.GetBytes(sInput);
+            byte[] hashArray = null;
+            // Encrypt Input string 
+            algorithmType = new MD5CryptoServiceProvider();
+            hashArray = algorithmType.ComputeHash(valueByteArr);
+            //Convert byte hash to HEX
+            StringBuilder sb = new StringBuilder();
+            foreach (byte b in hashArray)
+            {
+                sb.AppendFormat("{0:x2}", b);
+            }
+            return sb.ToString();
+        }
+        public static string Sha256(string data)
+        {
+            using (var sha256Hash = SHA256.Create())
+            {
+                // ComputeHash - returns byte array  
+                var bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(data));
+
+                // Convert byte array to a string   
+                var builder = new StringBuilder();
+                foreach (var t in bytes)
+                {
+                    builder.Append(t.ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+
+        public static string GetIpAddress(ActionExecutingContext context)
+        {
+            string ipAddress;
+            try
+            {
+                ipAddress = context.HttpContext.Request.Headers["HTTP_X_FORWARDED_FOR"].ToString();
+
+                if (string.IsNullOrEmpty(ipAddress) || ipAddress.ToLower() == "unknown")
+                    ipAddress = context.HttpContext.Request.Headers["REMOTE_ADDR"].ToString();
+            }
+            catch (Exception ex)
+            {
+                ipAddress = "Invalid IP:" + ex.Message;
+            }
+
+            return ipAddress;
+        }
+        public static String HmacSHA512(string key, String inputData)
+        {
+            var hash = new StringBuilder();
+            byte[] keyBytes = Encoding.UTF8.GetBytes(key);
+            byte[] inputBytes = Encoding.UTF8.GetBytes(inputData);
+            using (var hmac = new HMACSHA512(keyBytes))
+            {
+                byte[] hashValue = hmac.ComputeHash(inputBytes);
+                foreach (var theByte in hashValue)
+                {
+                    hash.Append(theByte.ToString("x2"));
+                }
+            }
+
+            return hash.ToString();
+        }
+    }
+    public class PayCompare : IComparer<string>
+    {
+        public int Compare(string? x, string? y)
+        {
+            if (x == y) return 0;
+            if (x == null) return -1;
+            if (y == null) return 1;
+            var Compare = CompareInfo.GetCompareInfo("en-US");
+            return Compare.Compare(x, y, CompareOptions.Ordinal);
+        }
+    }
+    public class PayLib
+    {
+        private SortedList<string, string> _requestData = new SortedList<string, string>(new PayCompare());
+        private SortedList<string, string> _responseData = new SortedList<string, string>(new PayCompare());
+
+        //------------------------REQUEST DATA----------------------------------------
+        public void AddRequestData(string key, string value)
+        {
+            if (!String.IsNullOrEmpty(value))
+            {
+                _requestData.Add(key, value);
+            }
+        }
+
+        public string CreateRequestUrl(string baseUrl, string vnp_HashSecret)
+        {
+            StringBuilder data = new StringBuilder();
+            foreach (KeyValuePair<string, string> kv in _requestData)
+            {
+                if (!String.IsNullOrEmpty(kv.Value))
+                {
+                    data.Append(kv.Key + "=" + HttpUtility.UrlEncode(kv.Value) + "&");
+                }
+            }
+            string queryString = data.ToString();
+            string rawData = GetRequestRaw();
+            baseUrl += "?" + queryString;
+            string vnp_SecureHash = Util.Sha256(vnp_HashSecret + rawData);
+            baseUrl += "vnp_SecureHash=" + vnp_SecureHash;
+            return baseUrl;
+        }
+        public string CreateRequestUrlWithHmacSHA512(string baseUrl, string vnp_HashSecret)
+        {
+            StringBuilder data = new StringBuilder();
+            foreach (KeyValuePair<string, string> kv in _requestData)
+            {
+                if (!String.IsNullOrEmpty(kv.Value))
+                {
+                    data.Append(WebUtility.UrlEncode(kv.Key) + "=" + WebUtility.UrlEncode(kv.Value) + "&");
+                }
+            }
+            string queryString = data.ToString();
+
+            baseUrl += "?" + queryString;
+            String signData = queryString;
+            if (signData.Length > 0)
+            {
+
+                signData = signData.Remove(data.Length - 1, 1);
+            }
+            string vnp_SecureHash = Util.HmacSHA512(vnp_HashSecret, signData);
+            baseUrl += "vnp_SecureHash=" + vnp_SecureHash;
+
+            return baseUrl;
+        }
+        private string GetRequestRaw()
+        {
+            StringBuilder data = new StringBuilder();
+            foreach (KeyValuePair<string, string> kv in _requestData)
+            {
+                if (!String.IsNullOrEmpty(kv.Value))
+                {
+                    data.Append(kv.Key + "=" + kv.Value + "&");
+                }
+            }
+            //remove last '&'
+            if (data.Length > 0)
+            {
+                data.Remove(data.Length - 1, 1);
+            }
+            return data.ToString();
+        }
+
+
+
+        //------------------------RESPONSE DATA----------------------------------------
+        public void AddResponseData(string key, string value)
+        {
+            if (!String.IsNullOrEmpty(value))
+            {
+                _responseData.Add(key, value);
+            }
+        }
+
+        public string GetResponseData(string key)
+        {
+            string retValue;
+            if (_responseData.TryGetValue(key, out retValue))
+            {
+                return retValue;
+            }
+            else
+            {
+                return string.Empty;
+            }
+        }
+
+        private string GetResponseRaw()
+        {
+
+            StringBuilder data = new StringBuilder();
+            if (_responseData.ContainsKey("vnp_SecureHashType"))
+            {
+                _responseData.Remove("vnp_SecureHashType");
+            }
+            if (_responseData.ContainsKey("vnp_SecureHash"))
+            {
+                _responseData.Remove("vnp_SecureHash");
+            }
+            foreach (KeyValuePair<string, string> kv in _responseData)
+            {
+                if (!String.IsNullOrEmpty(kv.Value))
+                {
+                    data.Append(kv.Key + "=" + kv.Value + "&");
+                }
+            }
+
+            //remove last '&'
+            if (data.Length > 0)
+            {
+                data.Remove(data.Length - 1, 1);
+            }
+            return data.ToString();
+        }
+
+        public bool ValidateSignature(string inputHash, string secretKey)
+        {
+            string rspRaw = GetResponseRaw();
+            string myChecksum = Util.Sha256(secretKey + rspRaw);
+            return myChecksum.Equals(inputHash, StringComparison.InvariantCultureIgnoreCase);
+        }
+        public bool ValidateSignatureHmacSHA512(string inputHash, string secretKey)
+        {
+            string rspRaw = GetResponseData();
+            string myChecksum = Util.HmacSHA512(secretKey, rspRaw);
+            return myChecksum.Equals(inputHash, StringComparison.InvariantCultureIgnoreCase);
+        }
+        private string GetResponseData()
+        {
+
+            StringBuilder data = new StringBuilder();
+            if (_responseData.ContainsKey("vnp_SecureHashType"))
+            {
+                _responseData.Remove("vnp_SecureHashType");
+            }
+            if (_responseData.ContainsKey("vnp_SecureHash"))
+            {
+                _responseData.Remove("vnp_SecureHash");
+            }
+            foreach (KeyValuePair<string, string> kv in _responseData)
+            {
+                if (!String.IsNullOrEmpty(kv.Value))
+                {
+                    data.Append(WebUtility.UrlEncode(kv.Key) + "=" + WebUtility.UrlEncode(kv.Value) + "&");
+                }
+            }
+            //remove last '&'
+            if (data.Length > 0)
+            {
+                data.Remove(data.Length - 1, 1);
+            }
+            return data.ToString();
+        }
+    }
+}
