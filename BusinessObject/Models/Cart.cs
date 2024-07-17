71d1fb723edb9632f38bb18296e26d7fae9007b8
Toanh <anhng130@gmail.com>
2024-07-17 11:22:38 +0700

    cart html

---
+++ BusinessObject/Models/Cart.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BusinessObject.Models
+{
+    public partial class Cart
+    {
+        public Cart()
+        {
+            CartDetails = new HashSet<CartDetail>();
+        }
+
+        public int Id { get; set; }
+        public DateTime OrderDate { get; set; }
+        public decimal? Freight { get; set; }
+
+        public virtual ICollection<CartDetail> CartDetails { get; set; }
+    }
+}
---
+++ BusinessObject/Models/CartDetail.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BusinessObject.Models
+{
+    public partial class CartDetail
+    {
+        public int CartId { get; set; }
+        public int ProductId { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+
+        public virtual Cart Cart { get; set; } = null!;
+        public virtual Product Product { get; set; } = null!;
+    }
+}
