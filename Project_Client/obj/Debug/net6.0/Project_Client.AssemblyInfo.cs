3cfe2ba8bbc0dc7352d3a8a4f915be58645dfcac
Toanh <anhng130@gmail.com>
2024-07-17 08:27:32 +0700

    ok

--- Project_Client/obj/Debug/net6.0/Project_Client.AssemblyInfo.cs
+++ Project_Client/obj/Debug/net6.0/Project_Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project_Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f0962370cbbf491ed2d5cf8625001d3ccd39f17")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project_Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project_Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
